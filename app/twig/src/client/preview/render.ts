7bcdffdcd2a5f71719f2a52a3a808c10e5641784
Fredrik Johansson <jeanfredrik@gmail.com>
2020-09-18 15:51:00 +0200

    feat: implement twig render

--- app/twig/src/client/preview/render.ts
+++ app/twig/src/client/preview/render.ts
@@ -15,24 +15,5 @@ export default function renderMain({
   const element = storyFn();
 
   showMain();
-  rootElement.innerHTML = 'YOLO';
-  // if (typeof element === 'string') {
-  //   rootElement.innerHTML = element;
-  // } else if (element instanceof Node) {
-  //   // Don't re-mount the element if it didn't change and neither did the story
-  //   if (rootElement.firstChild === element && forceRender === true) {
-  //     return;
-  //   }
-
-  //   rootElement.innerHTML = '';
-  //   rootElement.appendChild(element);
-  // } else {
-  //   showError({
-  //     title: `Expecting an HTML snippet or DOM node from the story: "${selectedStory}" of "${selectedKind}".`,
-  //     description: dedent`
-  //       Did you forget to return the HTML snippet from the story?
-  //       Use "() => <your snippet or node>" or when defining the story.
-  //     `,
-  //   });
-  // }
+  rootElement.innerHTML = element;
 }
