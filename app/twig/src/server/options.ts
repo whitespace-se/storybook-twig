a85035b13176ae72e47a6656535f1057d30c619e
Fredrik Johansson <jeanfredrik@gmail.com>
2020-09-20 19:47:53 +0200

    fix: missing explicit framework path

--- app/twig/src/server/options.ts
+++ app/twig/src/server/options.ts
@@ -4,5 +4,6 @@ const packageJson = require('../../package.json');
 export default {
   packageJson,
   framework: 'twig',
+  frameworkPath: '@whitespace/storybook-twig',
   frameworkPresets: [require.resolve('./framework-preset-twig.js')],
 };
